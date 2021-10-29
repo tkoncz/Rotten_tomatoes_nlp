dae55dcdad5532046ec03fe371af4c1718c5b94e
Tamas Koncz <tkoncz@emarsys.com>
2021-10-29 15:38:17 +0200

    ✨ scrape short reviews for franchise

--- scripts/scrape_franchise.R
+++ scripts/scrape_franchise.R
@@ -4,3 +4,19 @@ franchise_id <- "james_bond_007"
 
 franchise_movies <- getMoviesForFranchise(franchise_id)
 
+movie_ids <- franchise_movies[, movie_id]
+short_reviews <- purrr::map(movie_ids, ~{
+    Sys.sleep(1)
+    getShortReviews(.x)
+}) %>%
+    rbindlist()
+
+
+franchise_movie_reviews <- merge(
+    franchise_movies, short_reviews, by = "movie_id"
+)
+
+fwrite(
+    franchise_movie_reviews,
+    glue("data/{franchise_id}_franchise_short_reviews.csv")
+)
